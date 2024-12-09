8275ca83e251d8cf47e11debd56e5630b6c0f032
Samuel Born <samuelborn@outlook.de>
2024-12-09 18:08:07 +0100

    improve visualization script

--- scripts/visualize_seperator.py
+++ scripts/visualize_seperator.py
@@ -1,21 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-file_path = "./output/random_exp.txt"
+experiment = "same_degree"
+
+# Scatter plot data
 x_values = []
 y_values = []
-with open(file_path, 'r') as file:
-    for line in file:
+with open(f"./fragments/{experiment}.txt", 'r') as f:
+    for line in f:
         parts = line.strip().split()
         x_values.append(int(parts[0]))
         y_values.append(int(parts[1]))
-plt.scatter(x_values, y_values, color='blue', label='Data Points', zorder=5)
+plt.scatter(x_values, y_values, color='blue')
 
 # Plot cubic root function
 x_fit = np.linspace(0, max(x_values), 500)
 y_fit = np.cbrt(x_fit)
 plt.plot(x_fit, y_fit, color='red')
 
-# Plot settings
 plt.grid(True, linestyle='--', alpha=0.6)
+plt.savefig(f"fragments/{experiment}.svg", format='svg')
 plt.show()
