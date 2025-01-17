528c2efd2eabf40e1126d4640bd50e03ce24e1be
Samuel Born <samuelborn@outlook.de>
2025-01-17 21:37:32 +0100

    add kahip with correct compilation script as dependency

--- build.rs
+++ build.rs
@@ -1,5 +1,21 @@
+use std::path::Path;
+use std::process::Command;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=../KaHIP/deploy");
+    if !Path::new("./dependencies/KaHIP/deploy").exists() {
+        //panic!("{}", Path::new("./dependencies/KaHIP/deploy").display());
+        let status = Command::new("sh")
+            .arg("dependencies/KaHIP/compile_withcmake.sh")
+            .status()
+            .unwrap_or_else(|e| {
+                panic!("Failed to execute compilation script: {}", e);
+            });
+
+        if !status.success() {
+            panic!("Failed to compile KaHIP");
+        }
+    }
+
+    println!("cargo:rustc-link-search=native=./dependencies/KaHIP/deploy");
     println!("cargo:rustc-link-lib=kahip");
 }
-
