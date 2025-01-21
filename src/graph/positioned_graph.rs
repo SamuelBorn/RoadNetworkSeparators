e4df3ab9a77f48adc34989d3f656852295c7bcdb
Samuel Born <samuelborn@outlook.de>
2025-01-21 10:49:09 +0100

    load positioned graph from files

--- src/graph/positioned_graph.rs
+++ src/graph/positioned_graph.rs
@@ -1,3 +1,6 @@
+use std::io;
+
+use crate::library;
 use crate::Graph;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -20,5 +23,27 @@ impl PositionedGraph {
     pub fn new(graph: Graph, positions: Vec<Position>) -> PositionedGraph {
         assert_eq!(graph.get_num_nodes(), positions.len());
         PositionedGraph { graph, positions }
+    }
+
+    pub fn from_file(
+        first_out_file: &str,
+        head_file: &str,
+        latitude_file: &str,
+        longitude_file: &str,
+    ) -> io::Result<Self> {
+        let g = Graph::from_file(first_out_file, head_file)?;
+        let latitudes = library::read_binary_vec::<f32>(latitude_file)?;
+        let longitudes = library::read_binary_vec::<f32>(longitude_file)?;
+
+        assert_eq!(g.get_num_nodes(), latitudes.len());
+        assert_eq!(g.get_num_nodes(), longitudes.len());
+
+        let positions = latitudes
+            .into_iter()
+            .zip(longitudes)
+            .map(|(lat, lon)| Position(lat, lon))
+            .collect();
+
+        Ok(PositionedGraph::new(g, positions))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@ pub mod local;
 pub mod separator;
 
 use graph::{grid, Graph};
+use graph::positioned_graph::PositionedGraph;
 use separator::Mode::*;
 
 fn main() {
     //let g = Graph::from_file("../Graphs/karlsruhe/first_out", "../Graphs/karlsruhe/head").unwrap();
     //let g = Graph::from_file("../Graphs/germany/first_out", "../Graphs/germany/head").unwrap();
+    let g = PositionedGraph::from_file(
+        "../Graphs/germany/first_out",
+        "../Graphs/germany/head",
+        "../Graphs/germany/latitude",
+        "../Graphs/germany/longitude",
+    );
 
-    let g = Graph::from_edge_list_file("dependencies/BoltzmannPlanarGraphs/300k").unwrap();
-    g.recurse_separator(Fast, Some("output/planar.txt"));
+    //let g = Graph::from_edge_list_file("dependencies/BoltzmannPlanarGraphs/300k").unwrap();
+    //g.recurse_separator(Fast, Some("output/planar.txt"));
 }
