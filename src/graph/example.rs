fb8470a27d8835e452179151ba0b5f0384629456
Samuel Born <samuelborn@outlook.de>
2025-02-12 15:09:30 +0100

    add example graph loading

--- src/graph/example.rs
+++ src/graph/example.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use crate::graph::{geometric_graph::Position, Graph};
 
 use super::geometric_graph::GeometricGraph;
@@ -41,4 +43,20 @@ pub fn example_c4() -> GeometricGraph {
     ];
 
     GeometricGraph::new(g, positions)
+}
+
+pub fn karlsruhe() -> Graph {
+    Graph::from_file(Path::new("../Graphs/karlsruhe")).unwrap()
+}
+
+pub fn karlsruhe_server() -> Graph {
+    Graph::from_file(Path::new("/algoDaten/praktikum-ws-24-25/graph/karlsruhe")).unwrap()
+}
+
+pub fn germany() -> Graph {
+    Graph::from_file(Path::new("../Graphs/germany")).unwrap()
+}
+
+pub fn germany_server() -> Graph {
+    Graph::from_file(Path::new("/algoDaten/praktikum-ws-24-25/graph/germany")).unwrap()
 }
