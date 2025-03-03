14bd98a1b14aa91ffbea844004d120246c2bfc92
Samuel Born <samuelborn@outlook.de>
2025-03-03 19:36:39 +0100

    use voronoi in main

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ use std::path::Path;
 use cch::{chordalize_and_tree, get_directed_graph, get_subtree_sizes};
 use graph::delaunay::{length_restricted_delaunay, random_delaunay};
 use graph::geometric_graph::GeometricGraph;
+use graph::voronoi::voronoi_roadnetwork;
 use graph::{delaunay, example, grid, Graph};
 use library::{read_binary_vec, read_text_vec, read_to_usize_vec, write_binary_vec};
 use separator::Mode::*;
 
 fn main() {
-    let g = example::europe();
-    let ord = read_to_usize_vec(Path::new("output/ord_europe.bin"));
-    cch::compute_separator_sizes_from_order(&g, &ord);
+    voronoi_roadnetwork();
+    //let g = example::europe();
+    //let ord = read_to_usize_vec(Path::new("output/ord_europe.bin"));
+    //cch::compute_separator_sizes_from_order(&g, &ord);
 }
