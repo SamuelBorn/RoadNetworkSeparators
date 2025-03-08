c7560eba9cbc4ebfd63796ec62b5b57e3c5cf475
Samuel Born <samuelborn@outlook.de>
2025-03-08 17:48:39 +0100

    add visualization of multiple highlight files

--- scripts/visualize_graph.py
+++ scripts/visualize_graph.py
@@ -41,20 +41,33 @@ def visualize(args: argparse.Namespace) -> None:
         for v in g.vertices():
             pos[v] = [longitude[int(v)], latitude[int(v)]]
 
+    highlight_colors = [
+        [0, 0.5882352941176471, 0.5098039215686274, 1],
+        [0.8745098039215686, 0.6078431372549019, 0.10588235294117647, 1],
+        [0.27450980392156865, 0.39215686274509803, 0.6666666666666666, 1],
+        [0.6392156862745098, 0.06274509803921569, 0.48627450980392156, 1],
+    ]
     vertex_color = g.new_vertex_property("vector<double>")
+    vertex_size = g.new_vertex_property("double")
     for v in g.vertices():
         vertex_color[v] = [0, 0, 0, 1]
+        vertex_size[v] = 4
     if args.highlight_nodes:
-        highlight_indices: list[int] = read_node_list(args.highlight_nodes)
-        for idx in highlight_indices:
-            vertex_color[idx] = [1, 0, 0, 1]
+        for file, color in zip(args.highlight_nodes, highlight_colors):
+            highlight_indices: list[int] = read_node_list(Path(file))
+            for idx in highlight_indices:
+                vertex_size[idx] = 64
+                if vertex_color[idx] != [0, 0, 0, 1]:
+                    vertex_color[idx] = [0, 0, 0, 1]
+                else:
+                    vertex_color[idx] = color
 
     graph_draw(
         g,
         pos=pos,
         output=str(args.output),
         output_size=(args.size, args.size),
-        vertex_size=4,
+        vertex_size=vertex_size,
         vertex_fill_color=vertex_color,
         edge_pen_width=1,
     )
@@ -64,7 +77,7 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("dirname", type=Path)
     parser.add_argument("--output", type=Path)
-    parser.add_argument("--highlight-nodes", type=Path)
+    parser.add_argument("--highlight-nodes", type=Path, nargs="*")
     parser.add_argument("--auto-layout", action="store_true")
     parser.add_argument("--size", type=int, default=2**13)
     args: argparse.Namespace = parser.parse_args()
