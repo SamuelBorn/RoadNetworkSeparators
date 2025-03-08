a05626b6f7cc09b0f50038449b03832f5eee65f3
Samuel Born <samuelborn@outlook.de>
2025-03-08 17:52:17 +0100

    working planarization code

--- src/graph/planar.rs
+++ src/graph/planar.rs
@@ -5,6 +5,7 @@ use rayon::iter::IntoParallelRefIterator;
 use rayon::prelude::*;
 use rstar::{PointDistance, RTree, RTreeObject, AABB};
 use std::collections::HashSet;
+use std::mem;
 
 use super::geometric_graph::GeometricGraph;
 
@@ -50,7 +51,31 @@ impl RTreeObject for Edge {
     }
 }
 
-pub fn find_intersections(g: &GeometricGraph) {
+fn custom_cmp(a: &Point, b: &Point) -> std::cmp::Ordering {
+    let x_diff = (a.x() - b.x()).abs();
+
+    if x_diff < EPS {
+        a.y().total_cmp(&b.y())
+    } else {
+        a.x().total_cmp(&b.x())
+    }
+}
+
+fn replace_with_intersections(g: &mut GeometricGraph, edge: &Edge, intersections: &mut Vec<usize>) {
+    let (u, v) = edge.nodes;
+    intersections.push(u);
+    intersections.push(v);
+    intersections.sort_unstable_by(|&a, &b| custom_cmp(&g.positions[a], &g.positions[b]));
+
+    g.graph.remove_edge(u, v);
+    for window in intersections.windows(2) {
+        let (i, j) = (window[0], window[1]);
+        assert!(i != j);
+        g.graph.add_edge(i, j);
+    }
+}
+
+pub fn planarize(g: &mut GeometricGraph) {
     let edges = g
         .graph
         .get_directed_edges()
@@ -64,10 +89,17 @@ pub fn find_intersections(g: &GeometricGraph) {
 
     let tree = RTree::bulk_load(edges.clone());
 
-    let mut edge_to_intersections: HashMap<Edge, Vec<Edge>> = HashMap::new();
-    let mut intersection_to_coord: HashMap<(Edge, Edge), Coord> = HashMap::new();
+    // maps interscetion to new node id
+    let mut intersection_to_coord: HashMap<(Edge, Edge), usize> = HashMap::new();
+    // maps edges to all new nodes that are created due to intersections on this edge
+    let mut edge_to_intersections: HashMap<Edge, Vec<usize>> = HashMap::new();
 
-    edges.iter().for_each(|&current| {
+    println!("edges: {}", edges.len());
+    edges.iter().enumerate().for_each(|(status, &current)| {
+        if status % 1000 == 0 {
+            println!("status: {}", status);
+        }
+
         tree.locate_in_envelope_intersecting(&current.line.envelope())
             .filter(|&&candidate| current < candidate)
             .for_each(|&candidate| {
@@ -79,31 +111,40 @@ pub fn find_intersections(g: &GeometricGraph) {
                         && intersection.distance_2(&candidate.line.start) > 1e-8
                         && intersection.distance_2(&candidate.line.end) > 1e-8
                     {
+                        let id = *intersection_to_coord
+                            .entry((current, candidate))
+                            .or_insert_with(|| g.add_position_with_new_node(intersection.into()));
                         let entry = edge_to_intersections
                             .entry(current)
                             .or_insert(Vec::new())
-                            .push(candidate);
+                            .push(id);
                         let entry = edge_to_intersections
                             .entry(candidate)
                             .or_insert(Vec::new())
-                            .push(current);
-                        intersection_to_coord.insert((current, candidate), intersection);
+                            .push(id);
                     }
                 }
             })
     });
+    println!("intersections: {}", intersection_to_coord.len());
 
-    println!("Found {} intersections", intersection_to_coord.len());
+    for (edge, mut intersections) in edge_to_intersections {
+        replace_with_intersections(g, &edge, &mut intersections);
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use std::path::Path;
+
     use crate::graph::{example, Graph};
 
     use super::*;
+    use crate::separator::Mode::*;
 
     #[test]
-    fn intersection_works() {
+    #[ignore]
+    fn intersection_simple() {
         let g = Graph::from_edge_list(vec![(0, 1), (2, 3), (4, 5), (0, 2), (0, 2)]);
         let pos = vec![
             Point::new(0.0, 0.0),
@@ -113,14 +154,17 @@ mod test {
             Point::new(2.0, 0.0),
             Point::new(2.0, 1.0),
         ];
-        let g = GeometricGraph::new(g, pos);
-        g.graph.info();
-        find_intersections(&g);
+        let mut g = GeometricGraph::new(g, pos);
+        planarize(&mut g);
+        g.graph.print();
     }
 
     #[test]
+    #[ignore]
     fn intersection_karlsruhe() {
-        let g = example::geometric_karlsruhe();
-        find_intersections(&g);
+        let mut g = example::geometric_germany();
+        planarize(&mut g);
+        g.save(Path::new("output/germany_planar"));
+        // germany #intersections: 101062
     }
 }
