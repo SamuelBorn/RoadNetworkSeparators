32367d8c21ab264140bc8d0dfcddb916212492dd
Samuel Born <samuelborn@outlook.de>
2025-03-08 18:10:52 +0100

    compare germany plnar vs nonplanar

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use std::fs;
 use std::path::Path;
 
 use cch::{chordalize_and_tree, compute_separator_sizes_from_order, get_subtree_sizes};
-use graph::delaunay::{length_restricted_delaunay, random_delaunay};
 use graph::geometric_graph::GeometricGraph;
 use graph::highway::build_highway_network;
 use graph::voronoi::{voronoi_example, voronoi_example_small};
@@ -17,4 +16,7 @@ use library::{read_binary_vec, read_text_vec, read_to_usize_vec, write_binary_ve
 use separator::Mode::*;
 
 fn main() {
+    let mut g = Graph::from_file(Path::new("./output/germany_planar/")).unwrap();
+    let mut ord = read_to_usize_vec(Path::new("./output/ord_germany_planar.bin"));
+    compute_separator_sizes_from_order(&g, &ord, Path::new("./output/sep_germany_planar.txt"));
 }
