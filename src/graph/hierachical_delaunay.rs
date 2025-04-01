cc3fd3cb6a31ee52811e283e16fbe465e1ded175
Samuel Born <samuelborn@outlook.de>
2025-04-01 13:18:32 +0200

    add hierachical delaunay

---
+++ src/graph/hierachical_delaunay.rs
@@ -0,0 +1,52 @@
+use super::geometric_graph::GeometricGraph;
+use crate::{
+    graph::{delaunay, geometric_graph::approx_dedup_points},
+    library,
+};
+use geo::Point;
+use rand::seq::SliceRandom;
+
+pub fn generate_hierachical_delaunay(
+    city_percentage: &[f64],
+    points_per_level: &[usize],
+    radii: &[f64],
+) -> GeometricGraph {
+    assert_eq!(city_percentage[0], 1.0);
+    let rng = &mut rand::thread_rng();
+    let mut points = vec![Point::new(1000.0, 1000.0)];
+
+    for i in 0..city_percentage.len() {
+        let chosen = points
+            .choose_multiple(rng, (city_percentage[i] * points.len() as f64) as usize)
+            .cloned()
+            .collect::<Vec<_>>();
+
+        for center in chosen {
+            points.append(&mut library::random_points_in_circle(
+                center,
+                radii[i],
+                points_per_level[i],
+            ));
+        }
+    }
+
+    delaunay::delaunay(&points)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn simple_generate_hierachical_delaunay() {
+        let city_percentage = vec![1.0, 0.1, 0.6, 0.5];
+        let points_per_level = vec![70, 30, 30, 30];
+        let radii = vec![750., 200., 30., 3.];
+        let g = generate_hierachical_delaunay(&city_percentage, &points_per_level, &radii);
+
+        g.visualize("hierachical_delaunay");
+        g.graph.info();
+        g.graph
+            .recurse_separator(crate::separator::Mode::Fast, None);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -18,36 +18,4 @@ use rayon::prelude::*;
 use separator::{get_ord, Mode::*};
 use std::path::Path;
 
-fn main() {
-    //let city_percentage = vec![1.0, 0.4, 0.4, 0.4];
-    //let radii = vec![1500.0, 400.0, 60.0, 10.0];
-    //let points_per_level = vec![1000, 270, 50, 10];
-
-    [50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
-        .par_iter()
-        .for_each(|&last_radius| {
-            let city_percentage = vec![1.0, 0.5, 0.5, 0.5];
-            let radii = vec![last_radius as f64, 20.0, 5.0, 1.0];
-            let points_per_level = vec![50, 50, 50, 50];
-
-            let g = hierachical_disks::generate_circle_center_graph_v2(
-                &points_per_level,
-                &city_percentage,
-                &radii,
-            );
-
-            g.graph.info();
-            g.save(Path::new(format!("hierachical_disks_{}", last_radius).as_str()));
-            //g.inertial_flowcutter(format!("hierachical_disks_{}", last_radius).as_str());
-        });
-
-    
-    [50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
-        .par_iter()
-        .for_each(|&last_radius| {
-            let g = GeometricGraph::from_file(Path::new(format!("hierachical_disks_{}", last_radius).as_str())).unwrap();
-            g.inertial_flowcutter(format!("hierachical_disks_{}", last_radius).as_str());
-        });
-            
-
-}
+fn main() {}
