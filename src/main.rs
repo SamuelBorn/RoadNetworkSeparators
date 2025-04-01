a896321db2d85371dee07f39ee606e29f8ac9954
Samuel Born <samuelborn@outlook.de>
2025-04-01 08:36:50 +0200

    parallelize graph creation

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use graph::{geometric_graph::GeometricGraph, Graph};
 use library::{
     read_binary_vec, read_text_vec, read_to_usize_vec, write_binary_vec, write_text_vec,
 };
+use rayon::prelude::*;
 use separator::{get_ord, Mode::*};
 use std::path::Path;
 
@@ -22,18 +23,31 @@ fn main() {
     //let radii = vec![1500.0, 400.0, 60.0, 10.0];
     //let points_per_level = vec![1000, 270, 50, 10];
 
-    for last_radius in [50, 60, 70, 80, 90, 100, 110, 120, 130, 140] {
-        let city_percentage = vec![1.0, 0.5, 0.5, 0.5];
-        let radii = vec![last_radius as f64, 20.0, 5.0, 1.0];
-        let points_per_level = vec![50, 50, 50, 50];
+    [50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+        .par_iter()
+        .for_each(|&last_radius| {
+            let city_percentage = vec![1.0, 0.5, 0.5, 0.5];
+            let radii = vec![last_radius as f64, 20.0, 5.0, 1.0];
+            let points_per_level = vec![50, 50, 50, 50];
 
-        let g = hierachical_disks::generate_circle_center_graph_v2(
-            &points_per_level,
-            &city_percentage,
-            &radii,
-        );
+            let g = hierachical_disks::generate_circle_center_graph_v2(
+                &points_per_level,
+                &city_percentage,
+                &radii,
+            );
 
-        g.graph.info();
-        g.inertial_flowcutter(format!("hierachical_disks_{}", last_radius).as_str());
-    }
+            g.graph.info();
+            g.save(Path::new(format!("hierachical_disks_{}", last_radius).as_str()));
+            //g.inertial_flowcutter(format!("hierachical_disks_{}", last_radius).as_str());
+        });
+
+    
+    [50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+        .par_iter()
+        .for_each(|&last_radius| {
+            let g = GeometricGraph::from_file(Path::new(format!("hierachical_disks_{}", last_radius).as_str())).unwrap();
+            g.inertial_flowcutter(format!("hierachical_disks_{}", last_radius).as_str());
+        });
+            
+
 }
