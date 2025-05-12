5361a4defd7deb020b7c7066e2b3ed461dd2547a
Samuel Born <samuelborn@outlook.de>
2025-05-12 09:03:49 +0200

    add large scale experimnets

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub mod separator;
 use cch::{compute_separator_sizes_from_order, get_top_level_separator};
 use graph::example::{self, *};
 use graph::geometric_graph::GeometricGraph;
+use graph::hierachical_delaunay::random_pruned_hierachical_delaunay;
 use graph::Graph;
 use graph::{
     cbrt_maximal, delaunay, grid, hierachical_delaunay, hierachical_disks, highway, nested_grid,
@@ -19,13 +20,36 @@ use graph::{
 use separator::Mode::*;
 
 fn main() {
-    let mut g = hierachical_delaunay::pruned_hierachical_delaunay(
+    let g = hierachical_delaunay::pruned_hierachical_delaunay(
         &[1.0, 0.01, 0.5, 0.5],
         &[500, 30, 120, 100],
         &[5000., 500., 350., 20.],
     );
-    g.graph.info();
-    // g.visualize("hierachical_delaunay");
-    // g.graph.recurse_separator(Fast, None);
-    g.inertial_flowcutter("hierachical_delaunay");
+
+    for city_percentage in [0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9] {
+        let g = hierachical_delaunay::pruned_hierachical_delaunay(
+            &[1.0, 0.01, city_percentage, 0.5],
+            &[500, 30, 120, 100],
+            &[5000., 500., 350., 20.],
+        );
+        g.inertial_flowcutter(&format!("hierachical_delaunay_city_{}", city_percentage));
+    }
+
+    for points_per_level in [20, 80, 100, 120, 140, 160, 200] {
+        let g = hierachical_delaunay::pruned_hierachical_delaunay(
+            &[1.0, 0.01, 0.5, 0.5],
+            &[500, 30, points_per_level, 100],
+            &[5000., 500., 350., 20.],
+        );
+        g.inertial_flowcutter(&format!("hierachical_delaunay_points_{}", points_per_level));
+    }
+
+    for radii in [100, 200, 250, 300, 350, 400, 450, 550] {
+        let g = hierachical_delaunay::pruned_hierachical_delaunay(
+            &[1.0, 0.01, 0.5, 0.5],
+            &[500, 30, 120, 100],
+            &[5000., 500., radii as f64, 20.],
+        );
+        g.inertial_flowcutter(&format!("hierachical_delaunay_radii_{}", radii));
+    }
 }
