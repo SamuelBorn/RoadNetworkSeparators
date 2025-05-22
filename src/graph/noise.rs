9325014faba7b97e84e9fb231d6b20c6f80c9180
Samuel Born <samuelborn@outlook.de>
2025-05-22 11:24:09 +0200

    improve noise

--- src/graph/noise.rs
+++ src/graph/noise.rs
@@ -6,27 +6,30 @@ use rand::Rng;
 
 use crate::library;
 
-use super::{delaunay::delaunay, example::example_c4, geometric_graph::GeometricGraph, voronoi::prune_graph_parallel};
+use super::{
+    delaunay::delaunay,
+    example::example_c4,
+    geometric_graph::GeometricGraph,
+    voronoi::{prune_graph, prune_graph_parallel},
+};
 
-// assumes x, y are in [0, 1]
-fn should_place_point(x: f64, y: f64, perlin: &Perlin) -> bool {
-    let scale = 10.;
-    let noise = perlin.get([x * scale, y * scale]);
+fn should_place_point(p: &Point, perlin: &Perlin, scale: Option<f64>) -> bool {
+    let scale = scale.unwrap_or(10.0);
+    let noise = perlin.get([p.x() * scale, p.y() * scale]);
     let chance = rand::thread_rng().gen_range(-1.0..1.0);
+    let chance = chance * chance * chance;
     noise > chance
 }
 
-pub fn noise(n: usize) -> GeometricGraph {
+pub fn noise(n: usize, scale: Option<f64>) -> GeometricGraph {
     let mut p = Vec::with_capacity(n);
     let rng = &mut rand::thread_rng();
     let mut perlin = Perlin::new(0);
 
     while p.len() < n {
-        let x = rng.gen();
-        let y = rng.gen();
-
-        if should_place_point(x, y, &perlin) {
-            p.push(Point::new(x, y));
+        let x = library::random_point_in_circle(Point::new(0., 0.), 1.);
+        if should_place_point(&x, &perlin, scale) {
+            p.push(x);
         }
     }
 
@@ -34,6 +37,7 @@ pub fn noise(n: usize) -> GeometricGraph {
 
     let mut g = delaunay(&p);
     prune_graph_parallel(&mut g, 2.5);
+    // prune_graph(&mut g, 2.5);
     g.largest_connected_component()
 }
 
@@ -43,7 +47,15 @@ mod tests {
 
     #[test]
     fn noise_test() {
-        let g = noise(100_000);
-        g.inertial_flowcutter("noise");
+        // let g = noise(50_000, Some(5.0));
+        // g.inertial_flowcutter("noise");
+        // g.visualize("noise");
+        for i in 1..=10 {
+            let g = noise(50_000, Some(i as f64));
+            g.inertial_flowcutter(&format!("noise_{}", i));
+        }
     }
+
+    #[test]
+    fn noise_test_scale() {}
 }
