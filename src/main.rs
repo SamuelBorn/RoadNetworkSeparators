7a52d9edc12d702f9475cb19f43166eeaff2b92d
Samuel Born <samuelborn@outlook.de>
2025-05-27 09:43:27 +0200

    remove hier delaunay, in favor of noise

--- src/main.rs
+++ src/main.rs
@@ -21,37 +21,14 @@ use graph::hierachical_delaunay::random_pruned_hierachical_delaunay;
 use graph::Graph;
 use graph::{
     cbrt_maximal, delaunay, grid, hierachical_delaunay, hierachical_disks, highway, nested_grid,
-    tree, voronoi,
+    tree, voronoi, noise
 };
 use separator::Mode::*;
 
 fn main() {
-    let f1 = 1.0;
-    let f2 = 0.5;
-    let f3 = 0.3;
-    let f4 = 0.1;
+    let g = noise::noise_scales(5_000_000, &[4.0, 8.0, 16.0, 32.0]);
+    g.inertial_flowcutter("noise_low_only");
 
-    let s1 = 500;
-    let s2 = 150;
-    let s3 = 75;
-    let s4 = 35;
-
-    let r1 = 1000.0;
-    let r2 = 2.0 * r1 / (s1 as f64).sqrt();
-    let r3 = 1.5 * r2 / (s2 as f64).sqrt();
-    let r4 = 1.3 * r3 / (s3 as f64).sqrt();
-
-    println!("fract: {:.1}, {:.1}, {:.1}, {:.1}", f1, f2, f3, f4);
-    println!("sites: {:.0}, {:.0}, {:.0}, {:.0}", s1, s2, s3, s4);
-    println!("radii: {:.0}, {:.0}, {:.0}, {:.0}\n", r1, r2, r3, r4);
-
-    let g = hierachical_delaunay::pruned_hierachical_delaunay(
-        &[f1, f2, f3, f4],
-        &[s1, s2, s3, s4],
-        &[r1, r2, r3, r4],
-    );
-    g.graph.info();
-
-    g.inertial_flowcutter("delaunay_post_prune");
-    // g.visualize("tmp");
+    let g = noise::noise_scales(5_000_000, &[64.0, 128.0, 256.0, 512.0, 1024.0]);
+    g.inertial_flowcutter("noise_high_only");
 }
