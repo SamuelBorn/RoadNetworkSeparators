00856e632d4e7a25556217eddfbce35fc9f43ef7
Samuel Born <samuelborn@outlook.de>
2025-05-27 09:43:08 +0200

    extract noise scales into function

--- src/graph/noise.rs
+++ src/graph/noise.rs
@@ -24,27 +24,39 @@ fn should_place_point(p: &Point, perlin: &Perlin, scales: &[f64]) -> bool {
 }
 
 pub fn noise(n: usize) -> GeometricGraph {
+    let scales = [4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0];
+    noise_scales(n, &scales)
+}
+
+pub fn noise_scales(n: usize, scales: &[f64]) -> GeometricGraph {
     let mut p = Vec::with_capacity(n);
-    let rng = &mut rand::thread_rng();
-    let mut perlin = Perlin::new(rng.gen());
+    let mut perlin = Perlin::new(rand::thread_rng().gen());
 
-    let scales = vec![4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0];
-
+    let starttime = std::time::Instant::now();
     while p.len() < n {
-        p.append(
-            &mut library::random_points_in_circle(Point::new(0., 0.), 1., 1000)
-                .into_par_iter()
-                .filter(|p| should_place_point(p, &perlin, &scales))
-                .collect::<Vec<Point>>(),
-        );
+        let option = library::random_point_in_circle(Point::new(0., 0.), 1.);
+        if should_place_point(&option, &perlin, scales) {
+            p.push(option);
+        }
     }
-    // library::write_point_vec(Path::new("./output/noise_points"), &p);
+    println!(
+        "Points generated in {:.2} s",
+        starttime.elapsed().as_secs_f64()
+    );
 
-    println!("Points");
+    let starttime = std::time::Instant::now();
     let mut g = delaunay(&p);
-    println!("Delaunay");
+    println!(
+        "Delaunay triangulation in {:.2} s",
+        starttime.elapsed().as_secs_f64()
+    );
+
+    let starttime = std::time::Instant::now();
     prune_graph_parallel(&mut g, 2.5);
-    println!("Pruned");
+    println!(
+        "Pruning graph in {:.2} s",
+        starttime.elapsed().as_secs_f64()
+    );
     g.largest_connected_component()
 }
 
@@ -54,6 +66,6 @@ mod tests {
     #[test]
     fn noise_test() {
         let g = noise(1_000_000);
-        g.inertial_flowcutter("tmp");
+        // g.inertial_flowcutter("tmp");
     }
 }
