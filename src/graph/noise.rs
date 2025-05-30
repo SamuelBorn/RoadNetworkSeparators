1e075bc2369dfdd5dfeeb812730618f8d177c090
Samuel Born <samuelborn@outlook.de>
2025-05-30 10:43:54 +0200

    add fractal noise

--- src/graph/noise.rs
+++ src/graph/noise.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use geo::Point;
 use noise::{NoiseFn, Perlin};
-use rand::Rng;
+use rand::{thread_rng, Rng};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::library;
@@ -23,6 +23,19 @@ fn should_place_point(p: &Point, perlin: &Perlin, scales: &[f64]) -> bool {
     noise > 0.5f64.powi(scales.len() as i32)
 }
 
+fn should_place_point_fractal_noise(p: &Point, perlin: &Perlin, scales: &[f64]) -> bool {
+    let noise = scales
+        .iter()
+        .enumerate()
+        .map(|(i, &s)| {
+            let noise = perlin.get([p.x() * s + 3. * s, p.y() * s + 3. * s]) * 0.5 + 0.5;
+            noise * 1.0 / 2f64.powi(i as i32 + 1)
+        })
+        .sum::<f64>();
+
+    thread_rng().gen_bool(noise)
+}
+
 pub fn noise(n: usize) -> GeometricGraph {
     let scales = [8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0];
     noise_scales(n, &scales)
@@ -35,7 +48,8 @@ pub fn noise_scales(n: usize, scales: &[f64]) -> GeometricGraph {
     let starttime = std::time::Instant::now();
     while p.len() < n {
         let option = library::random_point_in_circle(Point::new(0., 0.), 1.);
-        if should_place_point(&option, &perlin, scales) {
+        if should_place_point_fractal_noise(&option, &perlin, scales) {
+        // if should_place_point(&option, &perlin, scales) {
             p.push(option);
         }
     }
