dd2ffe1658f30770e866bfaa473dc5d78e5d92f8
Samuel Born <samuelborn@outlook.de>
2025-06-25 16:54:49 +0200

    check prune edges

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use chrono::Local;
 use geo::Point;
 use graph::delaunay::delaunay;
 use graph::knn::knn;
+use graph::relative_neighborhood::relative_neighborhood;
 use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 use std::fs;
@@ -30,25 +31,26 @@ use graph::{
 fn main() {
     let mut num_iter = 1000;
 
-    let mut g = europe();
-    let n = g.get_num_nodes();
-    g.contract_and_llc();
-    g.hop_overview(num_iter, "hops_europe");
-    println!("{}\tHop overview done", Local::now());
-
-    let p = noise::get_noise_points(g.get_num_nodes());
-    let g = delaunay::delaunay(&p);
-    g.graph.hop_overview(num_iter, "hops_noise_delaunay");
-    println!("{}\tDelaunay triangulation done", Local::now());
-
-    let p = noise::get_noise_points(n);
+    // let mut g = europe();
+    // let n = g.get_num_nodes();
+    // g.contract_and_llc();
+    // g.hop_overview(num_iter, "hops_europe");
+    // println!("{}\tHop overview done", Local::now());
+    //
+    // let p = noise::get_noise_points(g.get_num_nodes());
+    // let g = delaunay::delaunay(&p);
+    // g.graph.hop_overview(num_iter, "hops_noise_delaunay");
+    // println!("{}\tDelaunay triangulation done", Local::now());
+    //
+    let p = noise::get_noise_points(18_000_000);
     let mut g = pruned_delaunay(&p, 2.5);
     g.graph.contract_and_llc();
-    g.graph.hop_overview(num_iter, "hops_noise_pruned");
+    // g.graph.hop_overview(num_iter, "hops_noise_pruned");
+    dbg!(g.graph.get_num_nodes());
     println!("{}\tPruning done", Local::now());
-
-    let mut g = relative_neighborhood::relative_neighborhood_points(p);
-    g.graph.contract_and_llc();
-    g.graph.hop_overview(num_iter, "hops_noise_rng");
-    println!("{}\tRelative neighborhood done", Local::now());
+    //
+    // let mut g = relative_neighborhood::relative_neighborhood_points(p);
+    // g.graph.contract_and_llc();
+    // g.graph.hop_overview(num_iter, "hops_noise_rng");
+    // println!("{}\tRelative neighborhood done", Local::now());
 }
