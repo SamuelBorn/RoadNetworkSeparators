7a17813ddad39f282019e7270796b74c16c83a5b
Samuel Born <samuelborn@outlook.de>
2025-06-25 14:16:34 +0200

    check custom prune graph

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::path::Path;
 
 use graph::example::*;
 use graph::geometric_graph::GeometricGraph;
-use graph::voronoi::prune_graph;
+use graph::voronoi::{prune_graph, prune_graph_parallel};
 use graph::Graph;
 use graph::{
     cbrt_maximal, delaunay, gabriel_graph, grid, hierachical_delaunay, hierachical_disks, highway,
@@ -29,6 +29,7 @@ use graph::{
 fn main() {
     let scales = &[4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0, 2048.0, 4096.0];
     let p = noise::get_noise_points_scales(18_000_000, scales);
-    let g = delaunay::delaunay(&p);
-    g.graph.hop_overview_contracted_bins(1000, 100, "noise_18m_hops_full_delaunay");
+    let mut g = delaunay::delaunay(&p);
+    prune_graph_parallel(&mut g, 2.5);
+    g.graph.hop_overview_contracted_bins(1000, 100, "noise_18m_hops_custom_prune");
 }
